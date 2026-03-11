1a29924cbb8edadf70ba8dcde9dc7d4059de9fbd
Thalia999 <thaliac000@gmail.com>
2026-03-11 12:16:37 -0400

    fix

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-const userRouter = require('./route/users')
+const userRouter = require('./route/user')
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('users', userRouter);
+app.use('/user', userRouter);
 
 app.get('/solution', (res, req) => {
   let num1 = parseInt(req.query['num1']);
