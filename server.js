3b0cc61aa918b10d778e9b24da17f4d3ce62b360
Thalia999 <thaliac000@gmail.com>
2026-03-11 11:36:17 -0400

    new code

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+app.get('/solution', (res, req) => {
+  let num1 = parseInt(req.query['num1']);
+  let num2 = parseInt(req.query['num2']);
+  let total = num1 + num2;
+  res.send(`<h2${num1} + ${num2} = ${total} </h2>`);
+});
 // GET /submit - logs query parameters
 app.get('/submit', (req, res) => {
   console.log('--- GET Request Received ---');
@@ -30,6 +36,7 @@ app.post('/submit', (req, res) => {
   res.send('<h2>POST request received!</h2><pre>' + JSON.stringify(req.body, null, 2) + '</pre>');
 });
 
-app.listen(3000, () => {
+
+app.listen(3030, () => {
   console.log('Server running on http://localhost:3000');
 });
